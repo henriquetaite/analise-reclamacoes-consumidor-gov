d00d8d989ce7c1aae380ff82d66e4fc0b1e2b347
Henrique Taite Mendes <henriquetaite@gmail.com>
2026-06-12 14:58:35 -0300

    Create 01_criacao_banco_tabela.sql

---
+++ sql/01_criacao_banco_tabela.sql
@@ -0,0 +1,7 @@
+-- ============================================================
+-- Projeto: Análise de Reclamações do Consumidor.gov.br
+-- Script: Criação do banco e da tabela principal
+-- Autor: Henrique Taite Mendes
+-- Objetivo: Criar estrutura inicial para importação dos dados de 2025
+-- ============================================================
+
