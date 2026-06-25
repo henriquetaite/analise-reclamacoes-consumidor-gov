d30cce5428d2f55f33f7b4d653c2f0677085fe25
Henrique Taite Mendes <henriquetaite@gmail.com>
2026-06-25 14:21:42 -0300

    Create 02_importacao_dados.sql

---
+++ sql/02_importacao_dados.sql
@@ -0,0 +1,109 @@
+USE consumidor_gov;
+
+-- Importação Janeiro
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Janeiro.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Fevereiro
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Fevereiro.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Março
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Março.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Abril
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Abril.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Maio
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Maio.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Junho
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Junho.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Julho
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Julho.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Agosto
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Agosto.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Setembro
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Setembro.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Outubro
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Outubro.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Novembro
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Novembro.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+-- Importação Dezembro
+LOAD DATA LOCAL INFILE 'C:/Users/henri/Projetos/analise-reclamacoes-consumidor-gov/dados/brutos/dadosgovbr---2025-Dezembro.csv'
+INTO TABLE reclamacoes_2025_bruto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
